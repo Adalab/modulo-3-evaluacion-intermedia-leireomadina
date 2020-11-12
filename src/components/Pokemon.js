8412330b772d640060bb5241ed0c5f71c10173bc
Leire Ordeñana <leireomadina@gmail.com>
2020-11-12 10:46:42 +0100

    Finish given styles

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,13 +4,13 @@ import "../stylesheets/_pokemon.scss";
 
 const Pokemon = (props) => {
   const pokemonType = props.pokemon.types.map((type, i) => {
-    return <li key={i}>{type}</li>;
+    return <li key={i} className="card-inner-list__item">{type}</li>;
   });
   return (
-    <article>
-      <img src={props.pokemon.url} alt={`imagen de ${props.pokemon.name}`} title={`imagen de ${props.pokemon.name}`} />
-      <h2>{props.pokemon.name}</h2>
-      <ul>{pokemonType}</ul>
+    <article className = "card-inner">
+      <img src={props.pokemon.url} alt={`imagen de ${props.pokemon.name}`} title={`imagen de ${props.pokemon.name}`} className = "card-inner__image"/>
+      <h2 className = "card-inner__title">{props.pokemon.name}</h2>
+      <ul className = "card-inner-list">{pokemonType}</ul>
     </article>
   );
 };
