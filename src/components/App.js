685e372571020232eb3574bdde1d813062efbc01
Leire Ordeñana <leireomadina@gmail.com>
2020-11-12 09:05:49 +0100

    Add components structure and json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
-import '../stylesheets/App.scss';
+import "../stylesheets/App.scss";
+import pokemons from "../data/pokemons.json";
+import PokeList from "./PokeList";
 
 function App() {
+  console.log(pokemons);
   return (
     <div className="App">
-      Hola mundo :)
+      <header>
+        <h1>Mi lista de Pokemon</h1>
+      </header>
+      <main>
+        <section>
+          <PokeList />
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/components/PokeList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PokeList = (props) => {
+  return (
+    <div>Pokemons</div>
+  );
+}
+
+export default PokeList;
